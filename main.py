c97cebebe31f100c9fc6ad92dbb8ef9c18e3a387
Kris TG <kketg03@gmail.com>
2023-10-04 17:34:13 -0400

    Began Layout construction of Editor Window

--- main.py
+++ main.py
@@ -1,16 +1,40 @@
 from PyQt5.QtWidgets import * 
 from PyQt5.QtGui import * 
+import sys
 
-app = QApplication([])
-window = QWidget()
-layout = QVBoxLayout()
-label1 = QLabel("HALP")
-label1.setFont(QFont("Arial", 24))
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    app.setStyle("Fusion")
+    window = QWidget()
+    container = QHBoxLayout()
+    left_side = QVBoxLayout()
+    container.addLayout(left_side)
+    right_side = QVBoxLayout()
+    container.addLayout(right_side)
+    window.setWindowTitle("Silly Data Object Packager")
+    window.setFixedWidth(1280)
+    window.setFixedHeight(720)
+    window.setLayout(container)
 
-layout.addWidget(label1)
-window.setWindowTitle("Prefabify")
-window.setFixedWidth(600)
-window.setFixedHeight(600)
-window.setLayout(layout)
-window.show()
-app.exec_()
+    name_label = QLabel("Name")
+    left_side.addWidget(name_label)
+    name_line = QLineEdit("Enter Name")
+    left_side.addWidget(name_line)
+    desc_label = QLabel("Description")
+    left_side.addWidget(desc_label)
+    desc_box = QTextEdit()
+    left_side.addWidget(desc_box)
+    data_label = QLabel("Data")
+    left_side.addWidget(data_label)
+
+    image_label = QLabel("Image")
+    right_side.addWidget(image_label)
+    default_image = QPixmap("default.png")
+    image_display = QLabel()
+    image_display.setPixmap(default_image.scaledToWidth(256))
+    image_display.resize(256,256)
+    right_side.addWidget(image_display)
+
+
+    window.show()
+    app.exec_()
